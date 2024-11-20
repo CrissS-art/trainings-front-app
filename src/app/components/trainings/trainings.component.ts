e498d41af7672f094c0ebe4c89f0d21d8151ae9c
Chris Le_Jeune <christophe.lejeune@fms-ea.com>
2024-11-20 13:29:44 +0100

    api rest implemented and functionnal

--- src/app/components/trainings/trainings.component.ts
+++ src/app/components/trainings/trainings.component.ts
@@ -1,8 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Training } from 'src/app/model/training.model.ts/training.model';
 import { CartService } from 'src/app/services/cart.service';
-
+import { ApiServiceService } from 'src/app/services/api-service.service';
 
 
 @Component({
@@ -15,17 +16,33 @@ import { CartService } from 'src/app/services/cart.service';
  * Composant de gestion des formations permettant l'affichage et l'ajout dans le panier de formation
  */
 export class TrainingsComponent implements OnInit {
-  listTrainings : Training[] | undefined;  
-  constructor(private cartService : CartService, private router : Router) {
+  listTrainings : Training[] | undefined;
+  error : String | null;
+
+  constructor(private cartService : CartService, 
+    private router : Router, 
+    private apiService: ApiServiceService) {
    }
 
   ngOnInit(): void {        
-    this.listTrainings = [ 
-      {id:1,name:'Java',description:'Formation Java SE 8 sur 5 jours',price:1500,quantity:1 },
-      {id:2,name:'DotNet',description:'Formation DotNet 3 jours',price:1000,quantity:1 },
-      {id:3,name:'Python',description:'Formation Python/Django 5 jours',price:1500,quantity:1 } 
-    ];
+    this.getAllTrainings();
   }
+
+/**Methode pour récupérer toutes les formations en lazy mode */
+getAllTrainings() {
+  this.apiService.getTrainings().subscribe({
+    next : (data) => {
+      console.log("Formations récupérées : ", data);
+      this.listTrainings = data.listTrainings
+    },
+    error: (err) => {
+      console.error('Erreur lors du chargement des formations:', err);
+      this.error = err.message;
+    },
+    complete : () => this.error = null
+  })
+}
+
 
   /**
    * Méthode permettant l'ajout d'une formation au panier en utilisant le service dédié
